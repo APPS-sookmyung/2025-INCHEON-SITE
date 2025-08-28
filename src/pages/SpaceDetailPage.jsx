155514b7d0b596accbd2ea565bbbf6665c91103b
임유진 <lyj5721@sookmyung.ac.kr>
2025-08-29 01:32:02 +0900

    feat: 상세 정보 부분 구현

--- src/pages/SpaceDetailPage.jsx
+++ src/pages/SpaceDetailPage.jsx
@@ -2,6 +2,8 @@ import {SPACE_DATA} from '../util/spaceInfo.js';
 import detailBackground1 from '../assets/DetailBackground1.svg';
 import detailBackground2 from '../assets/DetailBackground2.svg';
 import introDot from '../assets/introDot.svg';
+import detailDot from '../assets/DetailDot.svg';
+import mapIcon from '../assets/MapIcon.svg';
 
 import {useParams} from 'react-router-dom';
 
@@ -16,8 +18,22 @@ const SpaceDetailPage = () => {
     dong: '/DongDot.svg',
     jung: '/JungDot.svg',
   };
-
   const imageSrc = imageMap[section];
+
+  const DetailCard = ({title, children}) => (
+    <div>
+      <h3 className='text-lg pb-6 font-semibold border-b-2 border-black'>
+        {title}
+      </h3>
+      <div className='text-base mt-5'>{children}</div>
+    </div>
+  );
+
+  const dotColors = {
+    michuhol: 'bg-blue-600',
+    jung: 'bg-lime-600',
+    dong: 'bg-yellow-400',
+  };
 
   return (
     <div className='bg-[#FDFCF8] relative font-pretendard'>
@@ -31,30 +47,77 @@ const SpaceDetailPage = () => {
         />
       </div>
 
-      {/* intro */}
-      <div className='absolute flex top-0 w-full'>
-        <div className='pl-[150px] pr-[50px] pt-[180px] w-1/2'>
-          <h1 className='font-black text-6xl tracking-tighter mb-[80px]'>
-            {space.title}
-          </h1>
-          <span className='text-sm font-normal text-white bg-black rounded-full py-2 px-4'>
-            프로그램 소개
-          </span>
-          <p className='w-[80%] text-xl/10 text-left font-medium mt-[30px]'>
-            {space.program}
-          </p>
-          <img src={introDot} alt='introDot' className='h-[30px] mt-[80px]' />
+      {/* contents */}
+      <div className='relative w-full'>
+        {/* intro */}
+        <div className='flex w-full'>
+          <div className='pl-[150px] pr-[50px] pt-[180px] w-1/2'>
+            <h1 className='font-black text-6xl tracking-tighter mb-[80px]'>
+              {space.title}
+            </h1>
+            <span className='text-sm font-normal text-white bg-black rounded-full py-2 px-4'>
+              프로그램 소개
+            </span>
+            <p className='w-[80%] text-xl/10 text-left font-medium mt-[30px]'>
+              {space.program}
+            </p>
+            <img src={introDot} alt='introDot' className='h-[30px] mt-[80px]' />
+          </div>
+          <div className='relative w-1/2 h-[700px] overflow-hidden rounded-bl-full '>
+            <img
+              src={`${imagePath}/main.webp`}
+              alt='대표 이미지'
+              className='absolute inset-0 w-full h-full object-cover z-10 grayscale'
+            />
+            <img
+              src={imageSrc}
+              alt={section}
+              className='absolute -right-[20px] top-[50px] h-[95%] z-20'
+            />
+          </div>
         </div>
-        <div className='relative w-1/2 h-[700px] overflow-hidden rounded-bl-full '>
+
+        {/* detail */}
+        <div className='grid mt-[100px] px-[150px] grid-cols-1 md:grid-cols-3 gap-14'>
+          <DetailCard title='공간'>
+            <p>{space.location}</p>
+            <button
+              type='button'
+              className='mt-5 inline-flex items-center rounded-full border px-1 py-1 pr-4 text-sm'
+              // onClick={() => 길찾기 로직}
+            >
+              <img src={mapIcon} alt='mapIcon' className='pr-2 h-6' />
+              길찾기
+            </button>
+          </DetailCard>
+          <DetailCard title='공동 프로젝트 일정'>
+            <p>{space.projectSchedule}</p>
+          </DetailCard>
+          <DetailCard title='오픈스튜디오 정보 및 운영 일정'>
+            <p>
+              {space.studioInfo.map((group, groupIdx) => (
+                <div key={groupIdx} className='mb-8'>
+                  {group.map((item, i) => (
+                    <div key={i} className='flex pt-1 '>
+                      {i >= 2 && (
+                        <span
+                          className={`h-3 w-3 mt-1.5 mr-2 ml-8 rounded-full flex-shrink-0 ${
+                            dotColors[space.section]
+                          }`}>
+                          {' '}
+                        </span>
+                      )}
+                      <p className={i === 0 ? 'font-bold mb-1' : ''}>{item}</p>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </p>
+          </DetailCard>
           <img
-            src={`${imagePath}/main.webp`}
-            alt='대표 이미지'
-            className='absolute inset-0 w-full h-full object-cover z-10 grayscale'
-          />
-          <img
-            src={imageSrc}
-            alt={section}
-            className='absolute -right-[20px] top-[50px] h-[95%] z-20'
+            src={detailDot}
+            alt='detailDot'
+            className='h-[160px] ml-[100px]'
           />
         </div>
       </div>
