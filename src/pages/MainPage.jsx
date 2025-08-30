11f3c1fba4c9c23e7216d710f6c1507b563a08dc
suminb99 <suminbaek99@gmail.com>
2025-08-30 23:55:53 +0900

    #1 feat: MainPage에 Layout 및 Icon 컴포넌트 적용

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,19 @@
+import Icon from '../components/mainpage/icon';
+import Layout from '../components/mainpage/layout';
+
 const MainPage = () => {
-  return <div>메인페이지입니다!</div>;
+  return (
+    <div className='relative flex justify-center'>
+      <Layout>
+        <Icon
+          id='b1'
+          className='aboslute float-left z-1000 top-0 left-0'
+          width='140'
+          height='140'
+        />
+      </Layout>
+    </div>
+  );
 };
 
 export default MainPage;
