aecfd64e8aec9ce3d5add3bf9b482d48ed9f01db
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-09-01 17:18:40 +0900

    #4 feat: EventInfoSection에 yellow-dots 배치 및 레이아웃 적용

---
+++ src/components/mainpage/EventInfoSection.jsx
@@ -0,0 +1,16 @@
+import yellowDotsUrl from '../../assets/svg/yellow-dots.svg';
+
+const EventInfoSection = () => {
+  return (
+    <div className='absolute inset-0 pointer-events-none z-10'>
+      <div className='absolute top-[293px] left-0'>
+        <img
+          src={yellowDotsUrl}
+          alt='yellow dots'
+          className='w-[704px] h-[706px]'
+        />
+      </div>
+    </div>
+  );
+};
+export default EventInfoSection;
