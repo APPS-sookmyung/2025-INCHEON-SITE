0b2f683f4de9f7ef0ee6d41a88c638915b1abc28
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-09-01 17:21:11 +0900

    #4 feat: MainPage 레이아웃 및 주요 컴포넌트 구현

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,47 @@
 import background from '../assets/svg/background.svg';
 import Preview from '../components/mainpage/Preview';
+import EventScheduleSection from '../components/mainpage/EventScheduleSection';
+import EventInfoSection from '../components/mainpage/EventInfoSection';
 
 const MainPage = () => {
   return (
     <div className='flex justify-center py-15'>
-      <div className='relative w-full max-w-[1078px]'>
-        <img src={background} alt='background' />
-        <Preview />
+      <div className='w-full max-w-[1078px]'>
+        {/* PreviewSection */}
+        <div
+          className='relative w-full h-[1024px]'
+          style={{
+            backgroundImage: `url(${background})`,
+            backgroundPosition: '0 0',
+            backgroundSize: '1078px 5214px',
+            backgroundRepeat: 'no-repeat',
+          }}>
+          <Preview />
+        </div>
+
+        {/* EventScheduleSection */}
+        <div
+          className='relative w-full h-[2738px]'
+          style={{
+            backgroundImage: `url(${background})`,
+            backgroundPosition: '0 -1024px',
+            backgroundSize: '1078px 5214px',
+            backgroundRepeat: 'no-repeat',
+          }}>
+          <EventScheduleSection />
+        </div>
+
+        {/* EventInfoSection */}
+        <div
+          className='relative w-full h-[1452px]'
+          style={{
+            backgroundImage: `url(${background})`,
+            backgroundPosition: '0 -3762px',
+            backgroundSize: '1078px 5214px',
+            backgroundRepeat: 'no-repeat',
+          }}>
+          <EventInfoSection />
+        </div>
       </div>
     </div>
   );
