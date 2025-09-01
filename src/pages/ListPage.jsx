76bfacd3ff4569405b2233a744a28bcbd9a3e4c4
김영교 <ygkgy0105@gmail.com>
2025-09-01 14:48:27 +0900

    feat: 중간페이지 구현 #5

---
+++ src/pages/ListPage.jsx
@@ -0,0 +1,14 @@
+import { ArrowUpRight } from 'lucide-react';
+import { useLocation } from 'wouter';
+
+
+const ListPage = () => {
+    const [location, setLocation] = useLocation();
+
+    return (
+        <div>
+            <h1>ListPage</h1>
+        </div>
+    )
+
+}
