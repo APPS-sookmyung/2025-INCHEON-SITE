eb6620391d95a63f8782b72839fa074ec24e9a5f
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-09-04 15:07:24 +0900

    #4 feat: EventScheduleSection 도트 위치 및 색상 데이터 추가, SVG 배치 개선

--- src/components/mainpage/EventScheduleSection.jsx
+++ src/components/mainpage/EventScheduleSection.jsx
@@ -1,6 +1,25 @@
 import purpleDotsUrl from '../../assets/svg/purple-dots.svg';
 import greenDotsUrl from '../../assets/svg/green-dots.svg';
 
+const W = 1078;
+const H = 2738;
+
+const dotplaces = [
+  {x: 551, y: 422, color: 'green'},
+  {x: 715, y: 422, color: 'yellow'},
+  {x: 590, y: 822, color: 'yellow'},
+  {x: 868, y: 822, color: 'yellow'},
+  {x: 250, y: 1100, color: 'green'},
+  {x: 405, y: 1100, color: 'yellow'},
+  {x: 325, y: 1462, color: 'green'},
+  {x: 706, y: 1462, color: 'yellow'},
+  {x: 850, y: 1462, color: 'purple'},
+  {x: 325, y: 1820, color: 'yellow'},
+  {x: 504, y: 1820, color: 'green'},
+  {x: 678, y: 1820, color: 'purple'},
+  {x: 468, y: 2175, color: 'yellow'},
+];
+
 const EventScheduleSection = () => {
   return (
     <div className='absolute inset-0 pointer-events-none z-10'>
@@ -18,6 +37,24 @@ const EventScheduleSection = () => {
           className='w-[696px] h-[587px]'
         />
       </div>
+      {dotplaces.map((dot, i) => {
+        const left = (dot.x / W) * 100;
+        const top = (dot.y / H) * 100;
+
+        return (
+          <div
+            key={i}
+            className='absolute'
+            style={{
+              left: `${left}%`,
+              top: `${top}%`,
+            }}>
+            <svg width='40' height='134'>
+              <use href={`src/assets/svg/dotplace.svg#${dot.color}dotplace`} />
+            </svg>
+          </div>
+        );
+      })}
     </div>
   );
 };
