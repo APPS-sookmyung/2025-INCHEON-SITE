f48a36e1ae340b179cc7a83cd1856c3304f9ef7d
suminb99 <suminbaek99@gmail.com>
2025-10-07 19:32:13 +0900

    #12 style: 헤더 레이아웃 및 스타일 수정 - (창작공간들&설문조사 메뉴 가운데 정렬)

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -18,30 +18,26 @@ export default function Header() {
 
   return (
     <header className='sticky top-0 z-20 bg-[#F2F1EC]/80 backdrop-blur border-b border-neutral-300/70'>
-      <div className='mx-auto max-w-6xl px-5 h-16 flex items-center justify-between'>
-        <div className='flex items-center gap-6 '>
-          <Link to='/' onClick={() => goToMainAndScroll('top')}>
-            <img src={dotdotdot_logo} alt='logo' className='h-8' />
+      <div className='mx-auto max-w-6xl px-5 h-16 flex items-center justify-between text-base font-medium text-neutral-600'>
+        <Link to='/' onClick={() => goToMainAndScroll('top')}>
+          <img src={dotdotdot_logo} alt='logo' className='h-8' />
+        </Link>
+        <nav className='hidden md:flex gap-10'>
+          <Link className='hover:text-black hover:font-semibold' to='/spaces'>
+            창작 공간들
           </Link>
-          <nav className='hidden md:flex items-center gap-6 text-sm text-neutral-600'>
-            <Link className='hover:text-black' to='/spaces'>
-              창작 공간들
-            </Link>
-            <Link className='hover:text-black' to='/survey'>
-              설문 조사
-            </Link>
-          </nav>
-        </div>
-        <div className='flex items-center gap-3 text-neutral-600'>
-          <button
-            onClick={() => {
-              window.open('https://blog.naver.com/ifacpr');
-            }}
-            className='text-sm text-neutral-600 hover:text-black'>
-            <img src={blog} alt='블로그' className='w-6 h-6 md:hidden' />
-            <span className='hidden md:flex'>인천문화재단 블로그</span>
-          </button>
-        </div>
+          <Link className='hover:text-black hover:font-semibold' to='/survey'>
+            설문 조사
+          </Link>
+        </nav>
+        <button
+          onClick={() => {
+            window.open('https://blog.naver.com/ifacpr');
+          }}
+          className='cursor-pointer hover:text-black hover:font-semibold'>
+          <img src={blog} alt='블로그' className='w-6 h-6 md:hidden' />
+          <span className='hidden md:flex'>인천문화재단 블로그</span>
+        </button>
       </div>
     </header>
   );
