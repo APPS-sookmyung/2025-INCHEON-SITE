1534c01544e829047d64755129c27ccf7c03f9bc
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-10-23 19:45:54 +0900

    #21 build: Vite 경로 엘리어스 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
+import {fileURLToPath, URL} from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,4 +15,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
