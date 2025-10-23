9615b43cf168965d2917b346f064be178736ce6c
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-10-23 14:59:14 +0900

    #21 feat: footer 컴포넌트 구현 및 로고 svg 추가

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import incheonlogo from '../assets/svg/incheon-logo.svg';
+import incheonfoundationlogo from '../assets/svg/incheon-foundation-logo.svg';
 
 const Footer = () => {
   return (
-    <footer className="mt-8 border-t border-neutral-300 bg-[#D0CDC4] py-4 text-center text-sm text-neutral-500">
-      © {new Date().getFullYear()} 점점 All rights reserved.
+    <footer className='w-full bg-rose-200'>
+      <div className='max-w-7xl mx-auto h-56 px-4 lg:px-36 py-20 flex justify-between items-start'>
+        {/* 왼쪽 로고 그룹 */}
+        <div className='flex justify-start items-end gap-24'>
+          <img
+            className='w-28 h-6'
+            src={incheonlogo}
+            alt='인천 로고'
+            loading='lazy'
+          />
+          <img
+            className='w-28 h-7'
+            src={incheonfoundationlogo}
+            alt='인천재단 로고'
+            loading='lazy'
+          />
+        </div>
+
+        {/* 오른쪽 연락처 */}
+        <div className='w-[216px] h-7 flex items-center justify-start'>
+          <p className='text-type-head-2 text-2xl font-normal leading-loose'>
+            032)760-1089,1093
+          </p>
+        </div>
+      </div>
     </footer>
   );
 };
