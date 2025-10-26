73d3300b9ac898b375d2e597388a81bedf9d4906
suminb99 <suminbaek99@gmail.com>
2025-10-27 00:44:34 +0900

    #40 style: 그래픽 파일 교체 및 불필요한 속성 제거

--- src/components/sections/HeroPreview.jsx
+++ src/components/sections/HeroPreview.jsx
@@ -1,28 +1,15 @@
-import desktop from '@/assets/svg/graphic-desktop.svg';
-import tablet from '@/assets/svg/graphic-tablet.svg';
-import mobile from '@/assets/svg/graphic-mobile.svg';
-import logo from '@/assets/svg/dotdotdot-logo.svg';
+import main_graphic from '@/assets/svg/graphic-overview.svg'
 
 export default function Preview() {
   return (
-    <section className='relative w-full overflow-hidden'>
+    <section>
       <picture>
-        <source media='(max-width: 640px)' srcSet={mobile} />
-        <source media='(max-width: 1024px)' srcSet={tablet} />
         <img
-          src={desktop}
+          src={main_graphic}
           alt='인천 그래픽'
-          className='w-full h-auto object-contain'
+          className='object-contain'
         />
       </picture>
-
-      {/* 중앙 로고 */}
-      <img
-        src={logo}
-        alt='메인 로고'
-        className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
-                   w-[280px] sm:w-[400px] lg:w-[506px] h-auto'
-      />
     </section>
   );
 }
