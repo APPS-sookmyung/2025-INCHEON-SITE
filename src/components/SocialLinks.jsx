d10579f160a686002499f78e280313a2ee824b9f
suminb99 <suminbaek99@gmail.com>
2025-10-26 16:06:13 +0900

    #31 refactor: 헤더·풋터 공용 SocialLinks 컴포넌트 분리 및 재사용 구조 적용

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,44 @@
+import {Link} from 'react-router-dom';
+import blog from '@/assets/svg/blog-icon.svg';
+import instagram from '@/assets/svg/instagram-icon.svg';
+
+const SocialLinks = ({className = ''}) => {
+  return (
+    <div className={`${className}`}>
+      <div className='flex flex-1 justify-evenly'>
+        <a
+          href='https://www.instagram.com'
+          target='_blank'
+          rel='noopener noreferrer'>
+          <img
+            className='md:w-6 md:h-6 lg:w-7 lg:h-7'
+            src={instagram}
+            alt='인스타그램 아이콘'
+            loading='lazy'
+          />
+        </a>
+        <a
+          href='https://blog.example.com'
+          target='_blank'
+          rel='noopener noreferrer'>
+          <img
+            className='md:w-6 md:h-6 lg:w-7 lg:h-7'
+            src={blog}
+            alt='블로그 아이콘'
+            loading='lazy'
+          />
+        </a>
+      </div>
+
+      <Link
+        to='/official'
+        className='md:w-27 md:h-7 lg:w-28 lg:h-8 px-2.5 py-0.5 rounded-full outline-[1.2px] outline-type-head-2 flex justify-center items-center gap-2.5'>
+        <span className='text-center text-type-head-2 md:text-base lg:text-lg font-normal'>
+          공식 사이트
+        </span>
+      </Link>
+    </div>
+  );
+};
+
+export default SocialLinks;
