6063e964465f32ace8a0d2f72cfba4c4e9427c63
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-11-03 03:01:06 +0900

    #56 feat: places.js 업로드

---
+++ src/data/places.js
@@ -0,0 +1,88 @@
+export const PLACES = [
+  {
+    id: 'jangseung',
+    subtitle: '어벙또벙 이야기 수선집',
+    title: '장승',
+    content:
+      '우리 마을을 지켜주는 장승이에요. 낮을 가리는 두 장승이랑, 찾아오는 사람들에게 우리마을의 옛날 이야기를 들려줘요.',
+    image: '@/assets/svg/space-jangseung.svg',
+    hoverBgColor: 'var(--color-primary-4)',
+  },
+  {
+    id: 'teotbat',
+    subtitle: '창작집단 발아',
+    title: '텃밭',
+    content:
+      '우리 마을에 있는 싹이 튼 텃밭이에요. 작은 원인에 싸움을 바꾸려는 힘, 그 씨앗이 무엇으로 발아될지 궁금하죠?',
+    image: '@/assets/svg/space-teotbat.svg',
+    hoverBgColor: 'var(--color-primary-3)', 
+  },
+  {
+    id: 'eumakjangteo',
+    subtitle: '코드아트',
+    title: '음악장터',
+    content:
+      '텃밭을 지나면 음악 정터가 나와요! 특별한 날에는 예술가들의 사연이 담긴 공연을 판대요.',
+    image: '@/assets/svg/space-eumakjangteo.svg',
+    hoverBgColor: 'var(--color-primary-1)',
+  },
+  {
+    id: 'yeogwan',
+    subtitle: '올라이트컴퍼니',
+    title: '여관',
+    content:
+      '우리 마을에서 가장 오래된 여관은 의문의 시간이었어요. 진실을 알려줄 사람을 늘 찾고 있죠.',
+    image: '@/assets/svg/space-yeogwan.svg',
+    hoverBgColor: 'var(--color-primary-2)',
+  },
+  {
+    id: 'doltap',
+    subtitle: '모이소',
+    title: '돌탑',
+    content:
+      '뒷산으로 가면 돌탑이 나와요. 소원을 빌어봐요. 어두워지면 도깨비불도 볼 수 있대요!',
+    image: '@/assets/svg/space-doltap.svg',
+    hoverBgColor: 'var(--color-primary-3)',
+  },
+  {
+    id: 'dokkaebibul',
+    subtitle: '',
+    title: '도깨비불',
+    content:
+      '텃밭을 지나면 음악 정터가 나와요! 특별한 날에는 예술가들의 사연이 담긴 공연을 한대요.',
+    image: '@/assets/svg/space-dokkaebibul.svg',
+    hoverBgColor: 'var(--color-primary-2)',
+  },
+  {
+    id: 'golmok',
+    subtitle: '주가한술리',
+    title: '골목',
+    content: '골목 속으로 들어가면 아이들의 놀이소리와 음악소리가 크게 들려요.',
+    image: '@/assets/svg/space-golmok.svg',
+    hoverBgColor: 'var(--color-primary-1)',
+  },
+  {
+    id: 'munbanggu',
+    subtitle: '㈜위런드',
+    title: '문방구',
+    content: '골목 초입에 있는 주인아저씨 문방구는 아이들의 놀이터예요.',
+    image: '@/assets/svg/space-munbanggu.svg',
+    hoverBgColor: 'var(--color-primary-2)',
+  },
+  {
+    id: 'dabang',
+    subtitle: '카노오리연구소',
+    title: '다방',
+    content: '작은 다방에는 작은 조각보도 만들 수 있어요.',
+    image: '@/assets/svg/space-dabang.svg',
+    hoverBgColor: 'var(--color-primary-3)',
+  },
+  {
+    id: 'bangatgan',
+    subtitle: '몽예루틴',
+    title: '방앗간',
+    content: '골목에는 유명한 방앗간이 있는데 손님이 담긴 이야기가 펼쳐져요.',
+    image: '@/assets/svg/space-bangatgan.svg',
+    hoverBgColor: 'var(--color-primary-4)',
+  },
+];
