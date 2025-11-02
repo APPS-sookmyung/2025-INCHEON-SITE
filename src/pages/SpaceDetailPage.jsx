d5fd678de444d5e36ce24a2fe6c0bfb1d098812c
suminb99 <suminbaek99@gmail.com>
2025-11-03 04:01:06 +0900

    #54 refactor: SpaceDetailPage 기본 레이아웃 및 반응형 UI 마크업 구현

--- src/pages/SpaceDetailPage.jsx
+++ src/pages/SpaceDetailPage.jsx
@@ -1,180 +1,34 @@
-import {SPACE_DATA} from '../utils/spaceInfo.js';
-import textureTile from '@/assets/svg/texture-tile.svg';
-import {SPACE_IMAGES} from '../data/SpaceImages.jsx';
-import surveyButton from '../assets/svg/survey-button.svg';
-import {INFO_HIGHLIGHT} from '../data/InfoHighlights.jsx';
-import {DETAIL_HIGHLIGHT} from '../data/DetailHighlights.jsx';
-import {TITLE_HIGHLIGHTS} from '../data/TItleHighlights.jsx';
+// import textureTile from '@/assets/svg/texture-tile.svg';
 import {useParams} from 'react-router-dom';
+import {spaceEvents} from '../data/spaceEvents';
 
 const SpaceDetailPage = () => {
   const {id} = useParams();
-  //const imagePath = `/assets/${1}`; // 임의로 "1" 폴더만 사용
-  const space = SPACE_DATA.find((item) => item.id === Number(id));
-  // const section = space?.section;
-  //const imagekey=space.image;
-  // const imageMap = {
-  //   michuhol: '/detail/MichuholDot.svg',
-  //   dong: '/detail/DongDot.svg',
-  //   jung: '/detail/JungDot.svg',
-  // };
-
-  const imageSrc = SPACE_IMAGES[id];
-  const titleSrc = TITLE_HIGHLIGHTS[id];
-  const infoHighlightSrc = INFO_HIGHLIGHT[id];
-  const detailHighlightSrc = DETAIL_HIGHLIGHT[id];
-
-  const DetailCard = ({title, children}) => (
-    <div>
-      <h3 className='text-lg pb-6 font-semibold border-b-[1.8px] border-black'>
-        {title}
-      </h3>
-      <div className='text-base pt-5 relative'>{children}</div>
-    </div>
-  );
+  const event = spaceEvents.find((event) => Number(event.id) === Number(id));
 
   return (
-    <div
-      className='relative min-h-screen'
-      style={{
-        backgroundImage: `url(${textureTile})`,
-        backgroundRepeat: 'repeat',
-        backgroundSize: '90px 90px',
-      }}>
-      <div className='relative z-10 w-full px-6 pt-24 pb-16 lg:px-36 lg:pt-28 lg:pb-20'>
-        {/* 제목 */}
-        <div className='mb-12'>
-          <h1
-            className='text-4xl lg:text-[56px] font-normal tracking-tight mb-4'
-            style={{
-              fontFamily: 'ssYoungestDaughterRegular',
-              lineHeight: '1.1',
-              color: '#D5CFC2',
-            }}>
-            {space.title}
-          </h1>
-          {/* 부제목 */}
-          <div>
-            <p
-              className='text-xl lg:text-[24px] inline-block'
-              style={{
-                backgroundImage: `url(${titleSrc})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'auto 100%',
-                backgroundPosition: 'left ',
-                lineHeight: '1.3',
-                padding: '0.2em 0.2em ',
-              }}>
-              {space.sub_title}
-            </p>
+    <div className='w-9/10 md:w-3/4 mx-auto min-h-screen py-15 md:py-24 lg:w-full lg:px-37 lg:py-32'>
+      <div className='flex flex-col text-black text-sm md:text-lg lg:text-2xl'>
+        <h1 className='text-5xl md:text-7xl lg:text-8xl font-family-ssYoungestDaughterRegular text-type-head-1 mb-4.5 md:mb-6 lg:mb-9'>
+          {event.name}
+        </h1>
+        <h3 className='text-lg md:text-2xl lg:text-4xl mb-11 md:mb-17 lg:mb-22'>{`< ${event.title} >`}</h3>
+
+        {/* 프로그램 소개 */}
+        <div className='whitespace-pre space-y-5 md:space-y-7 lg:space-y-14 mb-20 md:mb-30 lg:mb-40'>
+          <p>프로그램 소개</p>
+          <p className='text-type-head-2'>{event.description}</p>
+        </div>
+
+        {/* 시간 + 장소 */}
+        <div className='flex gap-15 md:gap-30 lg:gap-48'>
+          <div className='space-y-5 md:space-y-7 lg:space-y-14'>
+            <p>시간</p>
+            <p className='text-type-head-2'>{event.time}</p>
           </div>
-        </div>
-
-        <div className='flex mt-10 lg:mt-16 relative'>
-          {/* 왼쪽 정보 영역 */}
-          <div className='w-full lg:w-1/2 lg:pr-10'>
-            {/* 프로그램 소개 */}
-            <div>
-              <h3
-                className='text-lg font-semibold  mb-3'
-                style={{
-                  backgroundImage: `url(${infoHighlightSrc})`,
-                  backgroundRepeat: 'no-repeat',
-                  backgroundSize: 'auto 110%',
-                  backgroundPosition: 'left ',
-                }}>
-                프로그램 소개
-              </h3>
-              <p className='text-base/7'>{space.program}</p>
-
-              {/* 프로그램 설명 */}
-              <div>
-                <p className='text-lg mt-4  font-bold text-type-head-2 tracking-[-0.75px] text-base md:text-lg lg:text-2xl'>
-                  {space.program_info}
-                </p>
-              </div>
-            </div>
-
-            {/* 시간 & 장소 */}
-            <div className='mt-10 flex flex-col gap-8 lg:flex-row lg:gap-16'>
-              <div>
-                <h3
-                  className='text-lg font-semibold  mb-3'
-                  style={{
-                    backgroundImage: `url(${detailHighlightSrc})`,
-                    backgroundRepeat: 'no-repeat',
-                    backgroundSize: 'auto 80%',
-                    backgroundPosition: 'left ',
-                  }}>
-                  시간
-                </h3>
-                <p className='text-lg mt-4  font-bold text-type-head-2 tracking-[-0.75px] text-base md:text-lg lg:text-2xl'>
-                  {space.time}
-                </p>
-              </div>
-
-              <div>
-                <h3
-                  className='text-lg font-semibold  mb-3'
-                  style={{
-                    backgroundImage: `url(${detailHighlightSrc})`,
-                    backgroundRepeat: 'no-repeat',
-                    backgroundSize: 'auto 80%',
-                    backgroundPosition: 'left ',
-                  }}>
-                  장소
-                </h3>
-                <p className='text-lg mt-4  font-bold text-type-head-2 tracking-[-0.75px] text-base md:text-lg lg:text-2xl'>
-                  {space.detail_location}
-                </p>
-              </div>
-            </div>
-
-            {/* 설문조사 버튼 (왼쪽 컬럼 내부 하단) */}
-            <div className='mt-12 '>
-              <button
-                onClick={() =>
-                  window.open(
-                    'https://linktr.ee/dot_dot_dot',
-                    '_blank',
-                    'noopener,noreferrer'
-                  )
-                }
-                className='absolute right-10 bottom-10 w-[150px]  cursor-pointer z-50'>
-                <img
-                  src={surveyButton}
-                  alt='설문조사 바로가기 버튼'
-                  className='w-[150px] h-auto'
-                />
-              </button>
-            </div>
-          </div>
-          {/* 오른쪽 이미지 영역 */}
-        </div>
-        <div className=' w-full absolute top-[20%] right-0 h-full lg:w-1/2 lg:block'>
-          <img
-            src={imageSrc}
-            alt={`${space.title} 대표 이미지`}
-            className='absolute top-[-220px] right-[-200px] w-[90%] h-auto opacity-40 max-w-none scale-100 z-10'
-            style={{transformOrigin: 'right top'}}
-          />
-        </div>
-      </div>
-      {/*지도*/}
-      <div className='mt-10 mb-[100px] relative px-6 md:px-12 lg:px-36'>
-        <div className='w-full lg:w-1/2 lg:pr-10'>
-          <div className='relative'>
-            <p
-              className='block text-lg font-semibold  w-full pb-[3%]'
-              style={{
-                backgroundImage: `url(${detailHighlightSrc})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'auto 80%',
-                backgroundPosition: 'auto ',
-              }}>
-              지도
-            </p>
-            <p className='w-full h-[350px] lg:h-[500px] m-0 bg-gray-100'></p>
+          <div className='space-y-5 md:space-y-7 lg:space-y-14'>
+            <p>장소</p>
+            <p className='text-type-head-2'>{event.address}</p>
           </div>
         </div>
       </div>
