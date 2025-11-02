c61abdd94f170184ccf06557f8260eb96d8a3d70
suminb99 <suminbaek99@gmail.com>
2025-11-02 16:46:07 +0900

    #55 feat: 오픈스튜디오 섹션 구현 (UI, 데이터 매핑, 반응형 포함)

---
+++ src/components/sections/StudioItem.jsx
@@ -0,0 +1,63 @@
+const StudioItem = ({studio}) => {
+  return (
+    <div className='w-full font-medium leading-8'>
+      <h3 className='text-type-body font-medium pt-5 pb-2'>
+        {studio.id} {studio.name}
+      </h3>
+      {studio.program ? (
+        <div className='grid grid-cols-2 gap-x-10 gap-y-3'>
+          <div>
+            <p className='text-type-body'>오픈스튜디오 프로그램</p>
+            <p
+              className='text-black'
+              style={{
+                WebkitTextStrokeWidth: '0.5px',
+              }}>
+              "{studio.program}"
+            </p>
+          </div>
+          <div>
+            <p className='text-type-body'>오픈 일자</p>
+            <p className='text-black whitespace-pre'>{studio.openDays}</p>
+          </div>
+          <div>
+            <p className='text-type-body'>오픈 시간</p>
+            <p className='text-black whitespace-pre'>{studio.openTime}</p>
+          </div>
+        </div>
+      ) : (
+        <div>
+          {studio.programs.map((program, index) => (
+            <div key={index} className='grid grid-cols-2 gap-5 pt-2 pb-2'>
+              <div>
+                <p className='text-type-body'>오픈스튜디오 프로그램</p>
+                <p
+                  className='whitespace-pre text-black'
+                  style={{
+                    WebkitTextStrokeWidth: '0.5px',
+                  }}>
+                  "{program.title}"
+                </p>
+              </div>
+              <div>
+                <p className='text-type-body'>오픈 일자</p>
+                <p className='whitespace-pre text-black'>{program.openDays}</p>
+              </div>
+              <div>
+                <p className='text-type-body'>
+                  {program.openTime ? '오픈 시간' : ''}
+                </p>
+                <p className='whitespace-pre text-black'>{program.openTime}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+      {studio.id < '10' && (
+        <div className='col-span-full h-1 bg-black' role='separator' />
+      )}
+    </div>
+  );
+};
+
+export default StudioItem;
