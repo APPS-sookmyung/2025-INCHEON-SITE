8e1bc607404b2b36571f08cb02395315d61b6b5c
suminb99 <suminbaek99@gmail.com>
2025-11-03 16:35:09 +0900

    #69 style: header extra 패딩 적용 + 메뉴 클릭 시 색상 변경

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -5,7 +5,7 @@ import instagram from '@/assets/svg/instagram-icon.svg';
 const SocialLinks = ({className = ''}) => {
   return (
     <div className={`${className}`}>
-      <div className='flex flex-1 justify-evenly'>
+      <div className='flex flex-1 justify-evenly *:p-1'>
         <a
           href='https://www.instagram.com/ifacpr/'
           target='_blank'
@@ -28,12 +28,12 @@ const SocialLinks = ({className = ''}) => {
             loading='lazy'
           />
         </a>
-        <div className='w-18 h-5 md:w-25 md:h-7 lg:w-28 lg:h-8 rounded-full outline-[1.2px] outline-type-head-2 hover:outline-type-body flex items-center justify-center text-center'>
+        <div className='w-18 h-5 md:w-25 md:h-7 lg:w-28 lg:h-8 rounded-full outline-[1.2px] outline-type-head-2 hover:outline-type-body md:outline-type-body md:hover:outline-type-head-2 flex items-center justify-center text-center'>
           <a
             href='https://ifac.or.kr/index.do'
             target='_blank'
             rel='noopener noreferrer'>
-            <p className='text-type-head-2 hover:text-type-body text-xs md:text-base lg:text-lg font-normal'>
+            <p className='text-type-head-2 hover:text-type-body text-xs md:text-type-body md:hover:text-type-head-2 md:text-base lg:text-lg font-normal'>
               공식 사이트
             </p>
           </a>
